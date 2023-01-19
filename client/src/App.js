91974dafa4834c545b0aa27219af59a8c0426997
rachel howard <rdfields13@gmail.com>
2023-01-19 12:03:29 -0600

    fixed login route

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Login from "./components/LoginForm";
+
 import SinglePost from "./pages/SingleThought";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -11,31 +13,6 @@ const client = new ApolloClient({
   uri: "/graphql",
   cache: new InMemoryCache(),
 });
-// function App() {
-//   return (
-//     <ApolloProvider client={client}>
-//       <Router>
-//         <div className="flex-column justify-flex-start min-100-vh">
-//           <Header />
-//           <div className="container">
-//             <Routes>
-//               <Route path="/" element={<Home />} />
-//               <Route path="/login" element={<Login />} />
-//               <Route path="/signup" element={<Signup />} />
-//               <Route path="/me" element={<Profile />} />
-//               <Route path="/profiles/:username" element={<Profile />} />
-//               <Route path="/thoughts/:thoughtId" element={<SingleThought />} />
-//             </Routes>
-//           </div>
-//           <Footer />
-//         </div>
-//       </Router>
-//     </ApolloProvider>
-//   );
-// }
-
-// export default App;
-
 function App() {
   return (
     <ApolloProvider client={client}>
@@ -45,8 +22,11 @@ function App() {
           <div className="container">
             <Routes>
               <Route path="/" element={<Home />} />
-              {/* Create a route to display a single thought's comments based on its `thoughtId` provided in the URL */}
-              <Route path="/post/:posttId" element={<SinglePost />} />
+              <Route path="/login" element={<Login />} />
+              {/* <Route path="/signup" element={<Signup />} /> */}
+              {/* <Route path="/me" element={<Profile />} /> */}
+              {/* <Route path="/profiles/:username" element={<Profile />} /> */}
+              <Route path="/posts/:postId" element={<SinglePost />} />
             </Routes>
           </div>
           <Footer />
--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { loginUser } from "../../utils/API";
+import { LOGIN_USER } from "../../utils/mutations";
 import Auth from "../../utils/auth";
 
 export default function Login() {
@@ -14,7 +14,7 @@ export default function Login() {
     event.preventDefault();
 
     try {
-      const response = await loginUser(userFormData);
+      const response = await LOGIN_USER(userFormData);
 
       if (!response.ok) {
         throw new Error("something went wrong!");
@@ -33,36 +33,69 @@ export default function Login() {
     });
   };
 
-    return (
-        <div class="login-border display-inline-block">
-            <section id="login" className="login">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-4" data-aos="fade-right">
-                            <div className="section-title">
-                                <h2 class="login-text">Login To Chipper!</h2>
-                            </div>
-                        </div>
-                        <div className="col-lg-8" data-aos="fade-up" data-aos-delay="100">
-                            <form action="forms/contact.php" method="post" role="form" className="php-email-form mt-4" onSubmit={handleFormSubmit}>
-                                <div className="row" class="input-space">
-                                    <div className="col-md-6 form-group mt-3 mt-md-0">
-                                        <input type="email" className="form-control" class="input-width"name="email" id="email" placeholder="Email" onChange={handleInputChange} value={userFormData.email} required />
-                                    </div>
-                                    <div className="col-md-6 form-group mt-3 mt-md-0">
-                                        <input type="password" className="form-control" class="input-width" name="password" id="password" placeholder="Password" onChange={handleInputChange} value={userFormData.password} required />
-                                    </div>
-                                </div>
-                                    <div className="col-md-6 form-group mt-3 mt-md-0">
-                                    </div>
-                                    <div className="col-md-6 form-group mt-3 mt-md-0">
-                                    </div>
-                                <div className="text-center"><button class="submit-button button-hover"disabled={!(userFormData.email && userFormData.password)} type="submit">Submit</button></div>
-                            </form>
-                        </div>
-                    </div>
+  return (
+    <div class="login-border display-inline-block">
+      <section id="login" className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4" data-aos="fade-right">
+              <div className="section-title">
+                <h2 class="login-text">Login To Chipper!</h2>
+              </div>
+            </div>
+            <div className="col-lg-8" data-aos="fade-up" data-aos-delay="100">
+              {/* eslint-disable-next-line */}
+              <form
+                action="forms/contact.php"
+                method="post"
+                role="form"
+                className="php-email-form mt-4"
+                onSubmit={handleFormSubmit}
+              >
+                <div className="row" class="input-space">
+                  <div className="col-md-6 form-group mt-3 mt-md-0">
+                    <input
+                      type="email"
+                      className="form-control"
+                      class="input-width"
+                      name="email"
+                      id="email"
+                      placeholder="Email"
+                      onChange={handleInputChange}
+                      value={userFormData.email}
+                      required
+                    />
+                  </div>
+                  <div className="col-md-6 form-group mt-3 mt-md-0">
+                    <input
+                      type="password"
+                      className="form-control"
+                      class="input-width"
+                      name="password"
+                      id="password"
+                      placeholder="Password"
+                      onChange={handleInputChange}
+                      value={userFormData.password}
+                      required
+                    />
+                  </div>
                 </div>
-            </section>
+                <div className="col-md-6 form-group mt-3 mt-md-0"></div>
+                <div className="col-md-6 form-group mt-3 mt-md-0"></div>
+                <div className="text-center">
+                  <button
+                    class="submit-button button-hover"
+                    disabled={!(userFormData.email && userFormData.password)}
+                    type="submit"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-    )
-};
+      </section>
+    </div>
+  );
+}
