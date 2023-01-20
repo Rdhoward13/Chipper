b54f58ae62efd4cbcebdf9e95bc697f8a54d9542
rachel howard <rdfields13@gmail.com>
2023-01-19 22:35:30 -0600

    fixed resolvers and typedefs on server side

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,26 +1,153 @@
-const { AuthenicationError } = require("apollo-server-express");
+// const { AuthenicationError } = require("apollo-server-express");
+// const { User, Post } = require("../models");
+// const { signToken } = require("../utils/auth");
+
+// const resolvers = {
+//   Query: {
+//     users: async () => {
+//       return User.find().populate("post");
+//     },
+//     posts: async (parent, { username }) => {
+//       return User.findOne({ username }).populate("posts");
+//     },
+//     post: async (parent, { username }) => {
+//       const params = username ? { username } : {};
+//       return Post.find(params).sort({ createdAt: -1 });
+//     },
+//     me: async (parent, args, context) => {
+//       if (context.user) {
+//         return User.findOne({ _id: context.user_id }).populate("posts");
+//       }
+//       throw new AuthenicationError("You need to be logged in!");
+//     },
+//   },
+//   Mutation: {
+//     addUser: async (parent, { username, email, password }) => {
+//       const user = await User.create({ username, email, password });
+//       const token = signToken(user);
+//       return { token, user };
+//     },
+//     login: async (parent, { email, password }) => {
+//       const user = await User.findOne({ email });
+//       if (!user) {
+//         throw new AuthenicationError("No user found with email address");
+//       }
+
+//       const correctPw = await user.isCorrectPassword(password);
+//       if (!correctPw) {
+//         throw new AuthenicationError("Incorrect crenditals");
+//       }
+//       const token = signToken(user);
+//       return { token, user };
+//     },
+//     // addPost: async (parent, { postText }, context) => {
+//     //   if (context.user) {
+//     //     const post = await Post.create({
+//     //       postText,
+//     //       postAuthor: context.user.username,
+//     //     });
+
+//     //     return post;
+//     //   }
+//     // throw new AuthenicationError("You need to be logged in!");
+//     addPost: async (parent, { postText }, context) => {
+//       if (context.user) {
+//         const post = await Post.create({
+//           postText,
+//           postAuthor: context.user.username,
+//         });
+
+//         await User.findOneAndUpdate(
+//           { _id: context.user._id },
+//           { $addToSet: { posts: post._id } }
+//         );
+
+//         return post;
+//       }
+//       throw new AuthenicationError("You need to be logged in!");
+//     },
+
+//     addComment: async (parent, { postId, commentText }, context) => {
+//       if (context.user) {
+//         return Post.findOneAndUpdate(
+//           { _id: postId },
+//           {
+//             $addToSet: {
+//               comments: { commentText, commentAuthor: context.user.username },
+//             },
+//           },
+//           {
+//             new: true,
+//             runValidators: true,
+//           }
+//         );
+//       }
+//       throw new AuthenicationError("You need to be logged in!");
+//     },
+//     removePost: async (parent, { postId }, context) => {
+//       if (context.user) {
+//         const post = await Post.findOneAndDelete({
+//           _id: postId,
+//           postAuthor: context.user.username,
+//         });
+//         await User.findOneAndUpdate(
+//           { _id: context.user._id },
+//           { $pull: { posts: post_id } }
+//         );
+//         return post;
+//       }
+//       throw new AuthenicationError("You need to be logged in!");
+//     },
+//     removeComment: async (parent, { postId, commentId }, context) => {
+//       if (context.user) {
+//         return Post.findOneAndUpdate(
+//           { _id: postId },
+//           {
+//             $pull: {
+//               comments: {
+//                 _id: commentId,
+//                 commentAuthor: context.user.username,
+//               },
+//             },
+//           },
+//           { new: true }
+//         );
+//       }
+//       throw new AuthenicationError("You need to be logged in!");
+//     },
+//   },
+// };
+
+// module.exports = resolvers;
+
+const { AuthenticationError } = require("apollo-server-express");
+// const { default: PostForm } = require("../../client/src/components/PostForm");
 const { User, Post } = require("../models");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     users: async () => {
-      return User.find().populate("post");
+      return User.find().populate("posts");
     },
-    posts: async (parent, { username }) => {
+    user: async (parent, { username }) => {
       return User.findOne({ username }).populate("posts");
     },
-    post: async (parent, { username }) => {
+    posts: async (parent, { username }) => {
       const params = username ? { username } : {};
       return Post.find(params).sort({ createdAt: -1 });
     },
+    post: async (parent, { postId }) => {
+      return Post.findOne({ _id: postId });
+    },
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user_id }).populate("posts");
+        return User.findOne({ _id: context.user._id }).populate("posts");
       }
-      throw new AuthenicationError("You need to be logged in!");
+      throw new AuthenticationError("You need to be logged in!");
     },
   },
+
   Mutation: {
     addUser: async (parent, { username, email, password }) => {
       const user = await User.create({ username, email, password });
@@ -29,27 +156,36 @@ const resolvers = {
     },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
+
       if (!user) {
-        throw new AuthenicationError("No user found with email address");
+        throw new AuthenticationError("No user found with this email address");
       }
 
       const correctPw = await user.isCorrectPassword(password);
+
       if (!correctPw) {
-        throw new AuthenicationError("Incorrect crenditals");
+        throw new AuthenticationError("Incorrect credentials");
       }
+
       const token = signToken(user);
+
       return { token, user };
     },
-    addPost: async (parent, { postText }, context) => {
+    addPost: async (parent, { thoughtText }, context) => {
       if (context.user) {
-        const post = await Post.create({
+        const post = await post.create({
           postText,
           postAuthor: context.user.username,
         });
 
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { posts: post._id } }
+        );
+
         return post;
       }
-      throw new AuthenicationError("You need to be logged in!");
+      throw new AuthenticationError("You need to be logged in!");
     },
     addComment: async (parent, { postId, commentText }, context) => {
       if (context.user) {
@@ -66,7 +202,7 @@ const resolvers = {
           }
         );
       }
-      throw new AuthenicationError("You need to be logged in!");
+      throw new AuthenticationError("You need to be logged in!");
     },
     removePost: async (parent, { postId }, context) => {
       if (context.user) {
@@ -74,13 +210,15 @@ const resolvers = {
           _id: postId,
           postAuthor: context.user.username,
         });
+
         await User.findOneAndUpdate(
           { _id: context.user._id },
-          { $pull: { posts: post_id } }
+          { $pull: { posts: post._id } }
         );
+
         return post;
       }
-      throw new AuthenicationError("You need to be logged in!");
+      throw new AuthenticationError("You need to be logged in!");
     },
     removeComment: async (parent, { postId, commentId }, context) => {
       if (context.user) {
@@ -97,7 +235,7 @@ const resolvers = {
           { new: true }
         );
       }
-      throw new AuthenicationError("You need to be logged in!");
+      throw new AuthenticationError("You need to be logged in!");
     },
   },
 };
